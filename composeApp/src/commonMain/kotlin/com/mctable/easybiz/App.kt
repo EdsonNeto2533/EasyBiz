2e4c4c0df3e4c59d07cc5ee1aa926a2da72a7445
Edson <edsonneto2533@gmail.com>
2026-02-02 17:07:23 -0300

    refactor: clean up unused imports in App.kt

--- composeApp/src/commonMain/kotlin/com/mctable/easybiz/App.kt
+++ composeApp/src/commonMain/kotlin/com/mctable/easybiz/App.kt
@@ -1,19 +1,6 @@
 package com.mctable.easybiz
 
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.safeContentPadding
-import androidx.compose.material3.Button
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.mctable.easybiz.core.ds.theme.EasyBizTheme
 import com.mctable.easybiz.core.navigation.AppNavHost
