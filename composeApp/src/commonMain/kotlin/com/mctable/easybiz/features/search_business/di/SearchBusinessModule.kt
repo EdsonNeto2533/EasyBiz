2d3ef9c8da0056145f70737306a1cb2e1510f2ff
Edson <edsonneto2533@gmail.com>
2026-03-03 10:42:51 -0300

    feat: add searchBusinessUseCase to DI module

--- composeApp/src/commonMain/kotlin/com/mctable/easybiz/features/search_business/di/SearchBusinessModule.kt
+++ composeApp/src/commonMain/kotlin/com/mctable/easybiz/features/search_business/di/SearchBusinessModule.kt
@@ -4,6 +4,8 @@ import com.mctable.easybiz.features.search_business.data.datasource.SearchBusine
 import com.mctable.easybiz.features.search_business.data.datasource.SearchBusinessDatasourceImpl
 import com.mctable.easybiz.features.search_business.data.repository.SearchBusinessRepositoryImpl
 import com.mctable.easybiz.features.search_business.domain.repository.SearchBusinessRepository
+import com.mctable.easybiz.features.search_business.domain.usecase.SearchBusinessUseCase
+import com.mctable.easybiz.features.search_business.domain.usecase.SearchBusinessUseCaseImpl
 import org.koin.dsl.module
 
 val searchBusinessModule = module {
@@ -13,5 +15,9 @@ val searchBusinessModule = module {
 
     single<SearchBusinessRepository> {
         SearchBusinessRepositoryImpl(get())
+    }
+
+    factory<SearchBusinessUseCase> {
+        SearchBusinessUseCaseImpl(get())
     }
 }
