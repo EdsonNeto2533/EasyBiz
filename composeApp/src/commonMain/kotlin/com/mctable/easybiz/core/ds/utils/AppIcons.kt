8ef23ed67e6494924feaf40a83c4fa1957c972ac
Edson <edsonneto2533@gmail.com>
2026-03-04 11:04:02 -0300

    feat: add new icons to the project and create TopAppBarOrganism.kt

--- composeApp/src/commonMain/kotlin/com/mctable/easybiz/core/ds/utils/AppIcons.kt
+++ composeApp/src/commonMain/kotlin/com/mctable/easybiz/core/ds/utils/AppIcons.kt
@@ -4,6 +4,8 @@ package com.mctable.easybiz.core.ds.utils
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.graphics.painter.Painter
 import easybiz.composeapp.generated.resources.Res
+import easybiz.composeapp.generated.resources.account_circle
+import easybiz.composeapp.generated.resources.arrow_back
 import easybiz.composeapp.generated.resources.contact_mail
 import easybiz.composeapp.generated.resources.visibility_off
 import easybiz.composeapp.generated.resources.visibility_on
@@ -24,5 +26,15 @@ object AppIcons {
     @Composable
     fun VisibilityOn(): Painter {
         return painterResource(Res.drawable.visibility_on)
+    }
+
+    @Composable
+    fun AccountCircle(): Painter {
+        return painterResource(Res.drawable.account_circle)
+    }
+
+    @Composable
+    fun ArrowBack(): Painter {
+        return painterResource(Res.drawable.arrow_back)
     }
 }
