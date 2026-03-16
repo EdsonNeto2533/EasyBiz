acd443062fff22934e5f015d8fb5dfcd6b089cb7
Edson <edsonneto2533@gmail.com>
2026-03-16 07:16:38 -0300

    feat: remove unused imports

--- composeApp/src/commonMain/kotlin/com/mctable/easybiz/features/register_business/presentation/ui/UpdateBusinessProfilePage.kt
+++ composeApp/src/commonMain/kotlin/com/mctable/easybiz/features/register_business/presentation/ui/UpdateBusinessProfilePage.kt
@@ -14,7 +14,6 @@ import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
