45181a44a83ebe0e128d47e9178b84f5af8b564c
Edson <edsonneto2533@gmail.com>
2026-04-25 15:12:48 -0300

    feat: adjust access token logic

--- composeApp/src/commonMain/kotlin/com/mctable/easybiz/features/auth/data/model/LoginResponseModel.kt
+++ composeApp/src/commonMain/kotlin/com/mctable/easybiz/features/auth/data/model/LoginResponseModel.kt
@@ -7,7 +7,7 @@ import kotlinx.serialization.Serializable
 data class LoginResponseModel(
     @SerialName("accessToken")
     val token: String,
-    @SerialName("id")
+    @SerialName("userId")
     val userId: String? = null,
     @SerialName("email")
     val email: String? = null
