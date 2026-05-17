8d98f1cf63aa0453b6b1754fc7be830358f50b72
Edson <edsonneto2533@gmail.com>
2026-05-17 12:54:37 -0300

    feat: remove unused imports

--- composeApp/src/commonMain/kotlin/com/mctable/easybiz/features/auth/presentation/event/VerifyEmailEvent.kt
+++ composeApp/src/commonMain/kotlin/com/mctable/easybiz/features/auth/presentation/event/VerifyEmailEvent.kt
@@ -1,9 +1,5 @@
 package com.mctable.easybiz.features.auth.presentation.event
 
-import com.mctable.easybiz.features.auth.presentation.state.OperationType
-import dev.icerock.moko.permissions.PermissionState
-import dev.icerock.moko.permissions.PermissionsController
-
 sealed class VerifyEmailEvent {
     data object SendCode : VerifyEmailEvent()
     data class OnEmailTyped(val email: String) : VerifyEmailEvent()
